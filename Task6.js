14c4a23b4c74464223367f018f353e86566f030f
AndriiYA <andrii.yasckov@gmail.com>
2017-10-03 18:54:54 +0300

    ===

--- Task6.js
+++ Task6.js
@@ -2,7 +2,7 @@
 var one =+ prompt('number', 100);
 var two =+ prompt("number", 100);
 var three =+ prompt("number", 100);
-if (one == two && two == three && three == one){
+if (one === two && two === three && three === one){
 console.log("yes");
 }else {
 console.log("ne ravni");
