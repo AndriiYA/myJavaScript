e8d35659931f364a58ee891571896caeee171303
AndriiYA <andrii.yasckov@gmail.com>
2017-10-04 23:19:49 +0300

    add task7

--- sameNumber_7.js
+++ sameNumber_7.js
@@ -1,5 +1,5 @@
 //Дано четырехзначное число. Определите, есть ли одинаковые цифры в нем.
-var one = prompt('number', 1000);
+/*var one = prompt('number', 1000);
 var a = Math.floor(one/1000);
 var b = Math.floor((one/100)%10);
 var c = Math.floor((one/10)%10);
@@ -13,3 +13,16 @@ console.log(" ravni ")}
 else{
 console.log("net ravnih");
 }
+*/
+var one = prompt('number', 100000000);
+var one = String(one);
+var arr = one.split('');
+		var i = 0, k = 0;
+		for (i = 0; i < arr.length; i++) {
+			for (k = arr.length; k > i; k--) {
+				if (arr[i] == arr[k]) {
+					console.log(" ravni ");
+                  break;
+				}
+			}
+		}
