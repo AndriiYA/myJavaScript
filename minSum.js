aefc8ed0d12f0a65670f6be35b9982d566eb0520
AndriiYA <andrii.yasckov@gmail.com>
2017-10-25 17:39:54 +0300

    Add minSum file korect

--- minSum.js
+++ minSum.js
@@ -1,15 +1,18 @@
 //Дан массив. Найдите два соседних элемента, сумма которых минимальна.
-		var array = [2,3,4,0,6,7,1,9];
+		var array = [2,3,4,0,6,7,1,-15];
 		var array2= [];
-		var sum = 0;
+		var sum = array[array.length-1] + array[0];
 		for (var i = 0; i<array.length; i++){
-		if (i % 2 == 0){
+		//if (i % 2 == 0){
+        array2.push(sum);
 		sum = array [i] + array[i + 1];
-		array2.push(sum);
-		}
+        
+		//}
 		}for (var a =0; a<array2.length; a++){
-        if (array2[a] < array2[a+1]){
-        var tmp = array2[a];
-        console.log(tmp);
-        } 
-        }
+        if(array2[a] < array2[0]){
+        array2[0] = array2[a];
+        //console.log(array2[a]);
+    }
+
+}
+console.log(array2[0]);
