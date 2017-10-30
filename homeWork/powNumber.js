cfbaa0b8e000a561b0b954e8c2871a8b040a6b4e
AndriiYA <andrii.yasckov@gmail.com>
2017-10-30 11:34:21 +0200

    Add rename

--- homeWork/powNumber.js
+++ homeWork/powNumber.js
@@ -2,12 +2,8 @@
 Пример var n = 2
 Результат: The sum is 5
 */
-//var number = 3;
-//for(i = 1; i <= number; i++){
-//console.log(Math.pow(i, 2));
-//}
-
 var number = 3;
-for (var i = 1; i<=number; i++){
-console.log(i*i);
-}
+var sum = 0;
+for(i = 1; i <= number; i++){
+sum += Math.pow(i, 2);
+}console.log(sum);
